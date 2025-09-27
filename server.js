1e3685c0d6aa9adf4dc5f4494e890f5c6b594e92
cryvennetwork <cryvennetwork@gmail.com>
2025-09-27 16:14:58 +0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("🚀 Cryven Mining App running on Node.js!");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
